3f891f5e9fe936f2d9c9f0c90f361cd299eb7228
wangyunXL <72874332+wangyunXL@users.noreply.github.com>
2023-12-02 09:41:25 +0800

    Update get_mulway_base_data.py

--- utils/get_mulway_base_data.py
+++ utils/get_mulway_base_data.py
@@ -41,7 +41,7 @@ if args.data_set == 'pascal':
 elif args.data_set == 'coco':
     num_classes = 80
 
-root_path = r"E:/WY/BAM_win/"
+root_path = r"../CFENet/"
 data_path = osp.join(root_path, 'data/base_annotation/')
 save_path = osp.join(data_path, args.data_set, args.mode, str(args.split))
 check_makedirs(save_path)
@@ -50,7 +50,7 @@ check_makedirs(save_path)
 sub_list, sub_val_list = get_train_val_set(args)
 
 # get data_list
-fss_list_root = root_path + '/BAM/lists/{}/fss_list/{}/'.format(args.data_set, args.mode)
+fss_list_root = root_path + '../CFENet/lists/{}/fss_list/{}/'.format(args.data_set, args.mode)
 fss_data_list_path = fss_list_root + 'data_list_{}.txt'.format(args.split)
 with open(fss_data_list_path, 'r') as f:
     f_str = f.readlines()
@@ -61,18 +61,18 @@ for line in f_str:
 
 # Start Processing
 for index in tqdm(range(len(data_list))):
-    image_path, label_path = data_list[index]  # 读取路径
-    image_path, label_path = root_path + image_path[3:], root_path + label_path[3:]  # 生成绝对路径
-    label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)  # 读取掩码
-    label_tmp = label.copy()  # 复制掩码
+    image_path, label_path = data_list[index]  
+    image_path, label_path = root_path + image_path[3:], root_path + label_path[3:]  
+    label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE) 
+    label_tmp = label.copy()  
 
-    for cls in range(1, num_classes + 1):  # 遍历类别索引
-        select_pix = np.where(label_tmp == cls)  # 找到掩码中包含每个类别的像素的位置
+    for cls in range(1, num_classes + 1):  
+        select_pix = np.where(label_tmp == cls)  
         if cls in sub_list:
-            label[select_pix[0], select_pix[1]] = sub_list.index(cls) + 1  # 获取掩码中类别在训练集类别中的索引并加1，将其复制给掩码中的相应位置
+            label[select_pix[0], select_pix[1]] = sub_list.index(cls) + 1  
         else:
-            label[select_pix[0], select_pix[1]] = 0  # 如果该类别不在训练集列表中，则将类别对应位置赋值为0
-            # 这可以避免在提取训练集图像中，图像中出现未知类别
+            label[select_pix[0], select_pix[1]] = 0  
+           
 
     # for pix in np.nditer(label, op_flags=['readwrite']):
     #     if pix == 255:
